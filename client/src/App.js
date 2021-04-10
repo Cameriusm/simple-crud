c547ea40b42b2fec25399a48ff4c96171e0886c8
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-04-10 22:50:06 +0700

    Add new employee

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
   const [name, setName] = useState('');
@@ -7,6 +8,18 @@ function App() {
   const [country, setCountry] = useState('');
   const [position, setPosition] = useState('');
   const [wage, setWage] = useState(0);
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log('Succes');
+    });
+  };
 
   return (
     <div className="App">
@@ -51,7 +64,7 @@ function App() {
           }}
           name=""
         />
-        <button>Add Employee</button>
+        <button onClick={addEmployee}>Add Employee</button>
       </div>
     </div>
   );
